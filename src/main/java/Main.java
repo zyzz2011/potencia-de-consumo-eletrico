238a6ca214bd9f872cd92f77f35666c5c6d339a3
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-05-28 01:41:33 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("potência e consumo eletrico");
+    System.out.println("potência é a força com que um aparelho elétrico trabalha no movimento dos eletrons.");
+    System.out.println("");
+    System.out.println(" -------------------");
+      
+      System.out.println("1000");
+      System.out.println("");
+      System.out.println("1. uma tv de 200 watts fica ligada aproximadamente 5h diariamente. Determine o consumo mensal da tv. (multiplicação)");
+
+
+System.out.println("200.5.30 = 30000");
+
+System.out.printl("30000;1000");
+    System.ot.println("30kwh");
+  }
+
+
+  
+}
